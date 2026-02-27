4e0171faf0cc3e66cd9e77ab62382e113d91e9cb
nev3rfail <nev3rfail@gmail.com>
2026-02-27 01:45:57 +0100

    generics

--- src/_py2dataclasses/type_utils.py
+++ src/_py2dataclasses/type_utils.py
@@ -1,13 +1,15 @@
 import abc
 import sys
+import six
 class _GenericMeta(abc.ABCMeta):
     def __getitem__(cls, typ):
         #n = type("{}[{}]".format(cls.__name__, typ.__name__), (cls,), {"__origin__":weakref.ref(cls), "__parameters__":typ})
         #n.__module__ = cls.__module__
         return cls(typ)
 
+@six.add_metaclass(_GenericMeta)
 class GenericAlias(object):
-    __metaclass__ = _GenericMeta
+    #__metaclass__ = _GenericMeta
     __slots__ = ('type',)
     def __init__(self, type_):
         self.type = type_
@@ -24,7 +26,7 @@ class GenericAlias(object):
         else:
             # typing objects, e.g. List[int]
             type_name = repr(self.type)
-        return 'type<{0}[{1}]>'.format(self.__class__, type_name)
+        return '{0}.{1}[{2}]'.format(self.__class__.__module__, self.__class__.__name__, type_name)
 #f = typing.GenericMeta
 
 def make_alias(name, * _types, **kwargs):
@@ -32,7 +34,8 @@ def make_alias(name, * _types, **kwargs):
         module = kwargs["module"]
     else:
         module = sys._getframe(1).f_globals.get('__name__', '__main__')
-    t = type("{}[{}]".format(name, ",".join(_types)), (GenericAlias,), {"__args__":_types})
+    #t = type("{}[{}]".format(name, ",".join(_types)), (GenericAlias,), {"__name__":name,"__args__":_types})
+    t = type("{}".format(name), (GenericAlias,),{}) #, {"__name__":name,"__args__":_types})
     #if module is not None:
     t.__module__ = module
     return t
