cfd398acb3cba94308c0c9ebad28a886a79c96b5
ascherbakhov <a.scherbakhov@gmail.com>
2026-05-20 17:01:09 +0200

    support package dataclasses import

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -10,11 +10,22 @@ class TestPublicImports(unittest.TestCase):
 
         self.assertTrue(hasattr(py2dataclasses, 'dataclass'))
 
+    def test_top_level_dataclasses_module_imports(self):
+        import dataclasses
+
+        self.assertTrue(hasattr(dataclasses, 'dataclass'))
+
     @unittest.skipIf(sys.version_info < (3,), 'src namespace package is Python 3 only')
     def test_package_py2dataclasses_module_imports(self):
         from src import py2dataclasses
 
         self.assertTrue(hasattr(py2dataclasses, 'dataclass'))
+
+    @unittest.skipIf(sys.version_info < (3,), 'src namespace package is Python 3 only')
+    def test_package_dataclasses_module_imports(self):
+        from src import dataclasses
+
+        self.assertTrue(hasattr(dataclasses, 'dataclass'))
 
     def test_singleton_types_are_atomic(self):
         import _py2dataclasses.dataclasses as impl
