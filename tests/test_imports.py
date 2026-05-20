d4c5025dc12750742f78a229aabf697b03f3f26a
ascherbakhov <a.scherbakhov@gmail.com>
2026-05-20 17:00:03 +0200

    support package py2dataclasses import

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,11 +1,18 @@
 from __future__ import absolute_import
 
 import unittest
+import sys
 
 
 class TestPublicImports(unittest.TestCase):
     def test_top_level_py2dataclasses_module_imports(self):
         import py2dataclasses
+
+        self.assertTrue(hasattr(py2dataclasses, 'dataclass'))
+
+    @unittest.skipIf(sys.version_info < (3,), 'src namespace package is Python 3 only')
+    def test_package_py2dataclasses_module_imports(self):
+        from src import py2dataclasses
 
         self.assertTrue(hasattr(py2dataclasses, 'dataclass'))
 
